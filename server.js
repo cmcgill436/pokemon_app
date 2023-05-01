c645ebff298bfc53d57e3777d0e297a79a483a63
cmcgill436 <candra.mcgill@gmail.com>
2023-05-01 02:42:16 -0500

    Show view shows req.params.id

--- server.js
+++ server.js
@@ -16,6 +16,10 @@ app.get("/pokemon", (req, res) => {
   res.render("Index", { pokemon });
 });
 
+app.get("/pokemon/:id", (req, res) => {
+  res.send(pokemon[req.params.id]);
+});
+
 // Listen
 app.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}`);
